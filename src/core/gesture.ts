3577af31bbad03ed4bf2ec4e0e7bc0078111d6f1
Luobata <602975805@qq.com>
2018-06-21 09:13:21 +0800

    add: hammerjs to fix gesture

--- src/core/gesture.ts
+++ src/core/gesture.ts
@@ -3,18 +3,24 @@
  */
 import { config } from '@/core/config';
 import img from '@/core/img';
+import Hammer from 'hammerjs';
 
 export default class Gesture {
     private canvas: HTMLCanvasElement;
 
+    private hammer: HammerManager;
+    private tapListener: HammerListener;
+
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
+        this.hammer = new Hammer(this.canvas);
         this.init();
         this.event();
     }
 
     public destroyed(): void {
         // event off
+        this.hammer.off('tap', this.tapListener);
     }
 
     private init(): void {
@@ -22,12 +28,10 @@ export default class Gesture {
     }
 
     private event(): void {
-        this.canvas.addEventListener(
-            'click',
-            (e: MouseEvent): void => {
-                img.SETCENTER(e.clientX, e.clientY);
-                config.emitter.emit('zoom');
-            },
-        );
+        this.tapListener = (e: HammerInput): void => {
+            img.SETCENTER(e.center.x, e.center.y);
+            config.emitter.emit('zoom');
+        };
+        this.hammer.on('tap', this.tapListener);
     }
 }
